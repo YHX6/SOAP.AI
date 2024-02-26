7f85e652a60eac536f417eb45127d27fed874649
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-02-26 13:23:33 -0500

    add buttons and toolbar

--- frontend/src/pages/DiagnoisPage.jsx
+++ frontend/src/pages/DiagnoisPage.jsx
@@ -1,16 +1,22 @@
 import MyEditor from "../components/MyEditor";
 import "../assets/css/diagnois.css"
+import SpeechButton from "../components/SpeechButton";
 
 function DiagnosisPage() {
     return ( 
         <div className="main">
             <div className="diagnois-page">
                 <div className="diagnois-page-left">
-                    <MyEditor></MyEditor>
+                    <MyEditor  showTools={true}></MyEditor>
+
+                    <div className="audio-btns">
+                        <SpeechButton></SpeechButton>
+                        <SpeechButton></SpeechButton>
+                    </div>
                 </div>
 
                 <div className="diagnois-page-right">
-                    <MyEditor></MyEditor>
+                    <MyEditor showTools={true}></MyEditor>
                 </div>
 
             </div>
